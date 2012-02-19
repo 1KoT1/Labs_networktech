7204f7ca2f20c1fdc91d0b52c47cae04a79fda5d
Василий Почкаенко <PochkaenkoV@yandex.ru>
2012-02-19 16:45:39 +0800

    Сделал обработчик нового подключения на сервере.

--- ST_Lab1_server/controler.cpp
+++ ST_Lab1_server/controler.cpp
@@ -5,7 +5,10 @@ Controler::Controler(QObject *parent) :
 	QObject(parent), tcpServer(new QTcpServer(this)){
 	connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newConection()));
 	tcpServer->listen(QHostAddress::Any, 3000);
+	qDebug()<<trUtf8("Ожидаю подключения.");
 }
 
 void Controler::newConection(){
+	QAbstractSocket * socket = tcpServer->nextPendingConnection();
+	qDebug()<<trUtf8("Получено соединение: %1:%2").arg(socket->peerAddress().toString()).arg(socket->peerPort());
 }
