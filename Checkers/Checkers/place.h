54f7158ce8402b8d12e24484bbc39560a20a0df4
Василий Почкаенко <PochkaenkoV@yandex.ru>
2012-03-31 01:52:13 +0800

    Создал класс описывающий положение на доске.

---
+++ Checkers/Checkers/place.h
@@ -0,0 +1,18 @@
+#ifndef PLACE_H
+#define PLACE_H
+
+#include <QObject>
+
+class Place : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Place(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // PLACE_H
