6aa36cbff3e3ec78fd995d2f9e9cd0bf25786add
Василий Почкаенко <PochkaenkoV@yandex.ru>
2012-03-31 02:36:15 +0800

    Описал класс игры.

--- Checkers/Checkers/game.cpp
+++ Checkers/Checkers/game.cpp
@@ -1,6 +1,58 @@
 #include "game.h"
+#include "checker.h"
+#include "checkestate.h"
+#include "place.h"
 
 Game::Game(QObject *parent) :
-    QObject(parent)
+	QObject(parent), place(new Place(this))
 {
+}
+
+
+/** Запрос на начало игры.*/
+void Game::BeginOfGame(){
+
+}
+
+/** Перемещение шашки первым игроком.*/
+void Game::Move1(CheckerState from, CheckerState to){
+
+}
+
+/** Перемещение шашки вторым игроком.*/
+void Game::Move2(CheckerState from, CheckerState to){
+
+}
+
+/** Здаётся первый игрок. */
+void Game::EndOfGame1(){
+
+}
+
+/** Здаётся первый игрок. */
+void Game::EndOfGame2(){
+
+}
+
+/** Возможные ходы.
+ *
+ * Высылает сигнал с возможными ходами.
+ */
+void Game::PossibleMove(CheckerState) const{
+
+}
+
+/** Получить указатель на шашку по позиции. */
+Checker * Game::getCeckerByPosition(CheckerState) const{
+
+}
+
+/** Возможные ходы. */
+QVector<CheckerState> & Game::possibleMove(Checker) const{
+
+}
+
+/** переместить указанную шашку */
+void Game::move(Checker, CheckerState){
+
 }
--- Checkers/Checkers/game.h
+++ Checkers/Checkers/game.h
@@ -2,17 +2,62 @@
 #define GAME_H
 
 #include <QObject>
+#include <QVector>
 
+class CheckerState;
+class Checker;
+class Place;
+
+/** Игра
+ *
+ * Создаётся на сервере.
+ * @author Василий Почкаенко.
+ */
 class Game : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
+private:
+	/** Цвет первого игрока.*/
+	bool gamer1Color;
+
+	/** Позиция на доске.*/
+	Place * place;
 public:
-    explicit Game(QObject *parent = 0);
+	explicit Game(QObject *parent = 0);
 
 signals:
 
 public slots:
+	/** Запрос на начало игры.*/
+	void BeginOfGame();
 
+	/** Перемещение шашки первым игроком.*/
+	void Move1(CheckerState from, CheckerState to);
+
+	/** Перемещение шашки вторым игроком.*/
+	void Move2(CheckerState from, CheckerState to);
+
+	/** Здаётся первый игрок. */
+	void EndOfGame1();
+
+	/** Здаётся первый игрок. */
+	void EndOfGame2();
+
+	/** Возможные ходы.
+	 *
+	 * Высылает сигнал с возможными ходами.
+	 */
+	void PossibleMove(CheckerState) const;
+
+private:
+	/** Получить указатель на шашку по позиции. */
+	Checker * getCeckerByPosition(CheckerState) const;
+
+	/** Возможные ходы. */
+	QVector<CheckerState> & possibleMove(Checker) const;
+
+	/** переместить указанную шашку */
+	void move(Checker, CheckerState);
 };
 
 #endif // GAME_H
