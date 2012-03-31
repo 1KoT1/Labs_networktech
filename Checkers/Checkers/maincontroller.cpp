6086ee11a4b072e3643cdcc4863fee6827e6075a
Василий Почкаенко <PochkaenkoV@yandex.ru>
2012-03-31 20:02:36 +0800

    Описал класс главного контроллера.

--- Checkers/Checkers/maincontroller.cpp
+++ Checkers/Checkers/maincontroller.cpp
@@ -1,6 +1,18 @@
 #include "maincontroller.h"
+#include "game.h"
+#include "clientgame.h"
 
 MainController::MainController(QObject *parent) :
-    QObject(parent)
+	QObject(parent), game(0), clienGame(0)
 {
+}
+
+/** Начать игру как сервер.*/
+void StartServer(){
+
+}
+
+/** Начать игру как клиент.*/
+void ConnectToServer(QString){
+
 }
--- Checkers/Checkers/maincontroller.h
+++ Checkers/Checkers/maincontroller.h
@@ -3,15 +3,39 @@
 
 #include <QObject>
 
+class Game;
+class ClienGame;
+
+/** Главный контроллер.
+ *
+ * @author Василий Почкаенко
+ */
 class MainController : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
+private:
+	/** Игра.
+	 *
+	 * Если равен 0, игра не создана. Возможно мы — клиент.
+	 */
+	Game * game;
+
+	/** Подключение к игре на сервере.
+	 *
+	 * Если равен 0, значит не подключены к игре. Возможно мы — сервер.
+	 */
+	ClienGame clienGame;
 public:
-    explicit MainController(QObject *parent = 0);
+	explicit MainController(QObject *parent = 0);
 
 signals:
 
 public slots:
+	/** Начать игру как сервер.*/
+	void StartServer();
+
+	/** Начать игру как клиент.*/
+	void ConnectToServer(QString);
 
 };
 
