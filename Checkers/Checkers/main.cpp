2ea3414e0ac05e3305399cac76d95512d69cb4fa
Василий Почкаенко <PochkaenkoV@yandex.ru>
2012-03-31 20:04:26 +0800

    Поправил отступы.

--- Checkers/Checkers/main.cpp
+++ Checkers/Checkers/main.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/Checkers/main.qml"));
-    viewer.showExpanded();
+	QmlApplicationViewer viewer;
+	viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+	viewer.setMainQmlFile(QLatin1String("qml/Checkers/main.qml"));
+	viewer.showExpanded();
 
-    return app.exec();
+	return app.exec();
 }
